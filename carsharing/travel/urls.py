bb68462fbbe21b6d211832c1ffbc48f3e2e1b975
dancorfon <dancorfon@alum.us.es>
2024-02-28 13:41:15 +0100

    feat: Implementación de mapas

--- carsharing/travel/urls.py
+++ carsharing/travel/urls.py
@@ -4,6 +4,7 @@ from rest_framework.documentation import include_docs_urls
 from django.contrib.auth.views import LoginView
 from rest_framework_simplejwt.views import TokenRefreshView
 
+
 urlpatterns = [
     path('create/', views.create_travel),
     path('', views.get_travels),
@@ -11,5 +12,7 @@ urlpatterns = [
     path('edit/<int:id>/', views.edit_travel),
     path('delete/<int:id>/', views.delete_travel),
     path("my_travels/", views.get_travels_by_user),
-    
+    path('<str:lat1>,<str:long1>,<str:lat2>,<str:long2>',views.showroute,name='showroute'),
+    path('',views.showmap,name='showmap'),
+    path('obtener_latitud_longitud/', views.obtener_latitud_longitud, name='obtener_latitud_longitud'),
 ]
