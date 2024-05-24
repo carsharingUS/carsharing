c77e7db1d3db8027bcaf2c48af2c5e525a66fcd3
Fernando Baquero <fernandobaquero2002@gmail.com>
2024-05-24 18:18:16 +0200

    feat: cambio del frontend de cracion de viaje

--- carsharin client/src/pages/travels/TravelCreationPage.tsx
+++ carsharin client/src/pages/travels/TravelCreationPage.tsx
@@ -9,6 +9,23 @@ import { getCurrentUser } from "../../utils";
 import "./TravelCreationPage.css";
 import { OpenStreetMapProvider } from "leaflet-geosearch";
 import Navbar from "../../components/home/Navbar";
+import {
+  TextField,
+  Button,
+  IconButton,
+  InputAdornment,
+  Container,
+  Box,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  SvgIcon,
+} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import AddIcon from "@mui/icons-material/Add";
+import PersonIcon from "@mui/icons-material/Person";
+
 dayjs.locale("es");
 
 const TravelCreationPage = () => {
@@ -24,8 +41,8 @@ const TravelCreationPage = () => {
   const [estimated_duration, setEstimatedDuration] = useState<string>("");
   const [price, setPrice] = useState<number>(0);
   const [stops, setStops] = useState<string>("");
-  const [typingTimeout, setTypingTimeout] = useState<number>(0); // Nuevo estado para rastrear el tiempo de espera
-  const [selectedSeats, setSelectedSeats] = useState<number>(0); // Estado para almacenar la cantidad de plazas seleccionadas
+  const [typingTimeout, setTypingTimeout] = useState<number>(0);
+  const [selectedSeats, setSelectedSeats] = useState<number>(0);
 
   const navigate = useNavigate();
   const queryClient = useQueryClient();
@@ -63,9 +80,7 @@ const TravelCreationPage = () => {
 
   const handleOriginChange = (event: ChangeEvent<HTMLInputElement>) => {
     setOrigin(event.target.value);
-    // Reiniciar el tiempo de espera
     clearTimeout(typingTimeout);
-    // Establecer un nuevo tiempo de espera antes de realizar la búsqueda
     setTypingTimeout(
       setTimeout(
         () => searchAddresses(event.target.value, setOriginSuggestions),
@@ -76,9 +91,7 @@ const TravelCreationPage = () => {
 
   const handleDestinationChange = (event: ChangeEvent<HTMLInputElement>) => {
     setDestination(event.target.value);
-    // Reiniciar el tiempo de espera
     clearTimeout(typingTimeout);
-    // Establecer un nuevo tiempo de espera antes de realizar la búsqueda
     setTypingTimeout(
       setTimeout(
         () => searchAddresses(event.target.value, setDestinationSuggestions),
@@ -87,7 +100,7 @@ const TravelCreationPage = () => {
     );
   };
 
-  const handlePriceChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handlePriceChange = (event: ChangeEvent<HTMLInputElement>) => {
     const newNumber = parseFloat(event.target.value);
     setPrice(isNaN(newNumber) ? 0 : newNumber);
   };
@@ -98,17 +111,14 @@ const TravelCreationPage = () => {
 
   const handleSeatClick = (numSeats: number) => {
     setSelectedSeats((prevSelectedSeats) => {
-      // Si el mismo asiento está seleccionado, deselecciona
       if (prevSelectedSeats === numSeats) {
         return 0;
       } else {
-        // De lo contrario, selecciona el asiento
         return numSeats;
       }
     });
   };
 
-  // Función para buscar sugerencias de direcciones
   const searchAddresses = async (query, setSuggestions) => {
     const provider = new OpenStreetMapProvider({
       params: {
@@ -131,178 +141,132 @@ const TravelCreationPage = () => {
   };
 
   return (
-    <div>
+    <>
       <Navbar />
-      <div className="crear-viaje-overlay">
-        <div className="crear-viaje-modal">
-          <div className="crear-viaje-header">
-            <h3 className="crear-viaje-title">Create Travel</h3>
-            <Link
-              to="/"
-              className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-              data-modal-toggle="defaultModal"
-            >
-              <svg
-                aria-hidden="true"
-                className="w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              <span className="sr-only">Close modal</span>
-            </Link>
-          </div>
-          <form className="search-form" onSubmit={handleSubmit}>
-            <div className="input-group">
-              <label htmlFor="origin" className="label">
-                Origen
-              </label>
-              <input
-                value={origin}
-                type="text"
-                onChange={handleOriginChange}
-                name="origin"
-                autoComplete="off"
-                id="origin"
-                className="input"
-                placeholder="Calle Ejemplo, Ciudad Ejemplo, Provincia Ejemplo"
-              />
-              {originSuggestions.length > 0 && (
-                <div className="suggestions-container">
-                  <div className="suggestions">
-                    {originSuggestions.slice(0, 5).map((suggestion, index) => (
-                      <div
-                        key={index}
-                        className="suggestion"
-                        onClick={() => handleOriginSuggestionClick(suggestion)}
-                      >
-                        {suggestion?.label}
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              )}
-            </div>
-            <div className="input-group">
-              <label htmlFor="destination" className="label">
-                Destino
-              </label>
-              <input
-                value={destination}
-                type="text"
-                onChange={handleDestinationChange}
-                name="destination"
-                autoComplete="off"
-                id="destination"
-                className="input"
-                placeholder="Calle Ejemplo, Ciudad Ejemplo, Provincia Ejemplo"
-              />
-              {destinationSuggestions.length > 0 && (
-                <div className="suggestions-container">
-                  <div className="suggestions">
-                    {destinationSuggestions
-                      .slice(0, 5)
-                      .map((suggestion, index) => (
-                        <div
-                          key={index}
-                          className="suggestion"
-                          onClick={() =>
-                            handleDestinationSuggestionClick(suggestion)
-                          }
-                        >
-                          {suggestion?.label}
-                        </div>
-                      ))}
-                  </div>
-                </div>
-              )}
-            </div>
-            <div className="input-group">
-              <label htmlFor="price" className="label">
-                Precio
-              </label>
-              <input
-                value={price}
-                onChange={handlePriceChange}
-                type="number"
-                name="price"
-                step="0.01"
-                id="price"
-                className="input"
-                placeholder="1,50€"
-              />
-            </div>
-            <div className="input-group">
-              <label htmlFor="start_date" className="label">
-                Fecha
-              </label>
-              <input
-                type="datetime-local"
-                value={start_date}
-                onChange={handleDateChange}
-                className="input"
-              />
-            </div>
-            <div className="input-group">
-              <label htmlFor="seats" className="label">
-                Número de plazas
-              </label>
-              &nbsp; &nbsp; &nbsp;
-              <div className="seat-input-container">
-                <div className="seat-icons-container">
-                  {[1, 2, 3, 4].map((numSeats) => (
-                    <div
-                      key={numSeats}
-                      className={`seat-icon ${
-                        numSeats <= selectedSeats ? "selected" : ""
-                      }`}
-                      onClick={() => handleSeatClick(numSeats)}
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        width="24"
-                        height="24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        className="feather feather-user"
-                      >
-                        <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
-                        <circle cx="12" cy="7" r="4"></circle>
-                      </svg>
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </div>
-            <button type="submit" className="crear-viaje-button">
-              <svg
-                className="mr-1 -ml-1 w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
+
+      <Container>
+        <Box className="crear-viaje-modal">
+          <Box className="crear-viaje-header">
+            <Typography variant="h3" className="crear-viaje-title">
               Crear viaje
-            </button>
+            </Typography>
+            <IconButton
+              component={Link}
+              to="/"
+              aria-label="close"
+              className="close-modal"
+            >
+              <CloseIcon />
+            </IconButton>
+          </Box>
+          <form className="search-form" onSubmit={handleSubmit}>
+            <TextField
+              label="Origen"
+              value={origin}
+              onChange={handleOriginChange}
+              name="origin"
+              autoComplete="off"
+              fullWidth
+              margin="normal"
+              variant="outlined"
+              placeholder="Calle Ejemplo, Ciudad Ejemplo, Provincia Ejemplo"
+            />
+            {originSuggestions.length > 0 && (
+              <List className="suggestions-container">
+                {originSuggestions.slice(0, 5).map((suggestion, index) => (
+                  <ListItem
+                    button
+                    key={index}
+                    onClick={() => handleOriginSuggestionClick(suggestion)}
+                  >
+                    <ListItemText primary={suggestion.label} />
+                  </ListItem>
+                ))}
+              </List>
+            )}
+            <TextField
+              label="Destino"
+              value={destination}
+              onChange={handleDestinationChange}
+              name="destination"
+              autoComplete="off"
+              fullWidth
+              margin="normal"
+              variant="outlined"
+              placeholder="Calle Ejemplo, Ciudad Ejemplo, Provincia Ejemplo"
+            />
+            {destinationSuggestions.length > 0 && (
+              <List className="suggestions-container">
+                {destinationSuggestions.slice(0, 5).map((suggestion, index) => (
+                  <ListItem
+                    button
+                    key={index}
+                    onClick={() => handleDestinationSuggestionClick(suggestion)}
+                  >
+                    <ListItemText primary={suggestion.label} />
+                  </ListItem>
+                ))}
+              </List>
+            )}
+            <TextField
+              label="Precio"
+              value={price}
+              onChange={handlePriceChange}
+              type="number"
+              name="price"
+              fullWidth
+              margin="normal"
+              variant="outlined"
+              placeholder="1,50€"
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">€</InputAdornment>
+                ),
+              }}
+            />
+            <TextField
+              label="Fecha"
+              type="datetime-local"
+              value={start_date}
+              onChange={handleDateChange}
+              fullWidth
+              margin="normal"
+              variant="outlined"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+            <Box className="input-group">
+              <Typography className="label" style={{ marginRight: "1rem" }}>
+                Número de plazas
+              </Typography>
+              <Box className="seat-icons-container">
+                {[1, 2, 3, 4].map((numSeats) => (
+                  <Box
+                    key={numSeats}
+                    className={`seat-icon ${
+                      numSeats <= selectedSeats ? "selected" : ""
+                    }`}
+                    onClick={() => handleSeatClick(numSeats)}
+                  >
+                    <SvgIcon component={PersonIcon} />
+                  </Box>
+                ))}
+              </Box>
+            </Box>
+            <Button
+              type="submit"
+              color="primary"
+              variant="contained"
+              startIcon={<AddIcon />}
+              className="crear-viaje-button"
+            >
+              Crear viaje
+            </Button>
           </form>
-        </div>
-      </div>
-    </div>
+        </Box>
+      </Container>
+    </>
   );
 };
 
