69e41e5bc16cbfb41d6f41fb5e4a70c2748d452f
ferbaqfer <ferbaqfer@alum.us.es>
2024-06-24 16:43:21 +0200

    corregida la duplicidad del envío de mensajes

--- carsharing_client/src/api/ChatService.ts
+++ carsharing_client/src/api/ChatService.ts
@@ -3,47 +3,51 @@ import { ChatMessage, Message, Room, User } from "../Interfaces";
 import { API, authAPI } from "./AuthenticationService";
 
 // Usar en un futuro
-export const createWebsocket = async (roomId: number) => {
-  const socket = new WebSocket("ws://127.0.0.1:8000/ws/room/"+roomId+"/");
-  return socket;
-}
 
 // Send a get request to the api endpoint to get the message of the logged in user
-export const getMessages = async (roomId: string) : Promise<ChatMessage[]> => {
-  const response: AxiosResponse<ChatMessage[]> = await authAPI.get(`/chat/room/${roomId}/messages/`);
+export const getMessages = async (roomId: string): Promise<ChatMessage[]> => {
+  const response: AxiosResponse<ChatMessage[]> = await authAPI.get(
+    `/chat/room/${roomId}/messages/`
+  );
   return response.data;
-}
+};
 
 export const createMessage = async (data: Message) => {
-  const response = await authAPI.post(`/chat/room/create_message/`, data)
+  const response = await authAPI.post(`/chat/room/create_message/`, data);
   return response;
-}
+};
 
 export const createRoom = async (user1, user2) => {
   try {
-    const response = await API.post(`/chat/room/create_room/${user1}/${user2}/`);
+    const response = await API.post(
+      `/chat/room/create_room/${user1}/${user2}/`
+    );
     return response.data;
   } catch (error) {
-      throw new Error('Error al crear la sala de chat: ' + error.message);
+    throw new Error("Error al crear la sala de chat: " + error.message);
   }
-}
+};
 
 export const checkRoomExists = async (user1Id, user2Id) => {
   try {
-    const response = await API.get(`/chat/room/check_room_exists/${user1Id}/${2}`);
+    const response = await API.get(
+      `/chat/room/check_room_exists/${user1Id}/${2}`
+    );
     return response.data.room_exists;
   } catch (error) {
-    console.error('Error checking room existence:', error);
+    console.error("Error checking room existence:", error);
     return false;
   }
 };
 
 export const getOrCreateRoomByUsers = async (user1Id, user2Id) => {
   try {
-    const response = await API.get(`/chat/room/get_or_create_room/${user1Id}/${user2Id}/`);
+    const response = await API.get(
+      `/chat/room/get_or_create_room/${user1Id}/${user2Id}/`
+    );
     return response.data;
   } catch (error) {
-    console.error('Error checking room existence:', error);
+    console.error("Error checking room existence:", error);
     return false;
   }
 };
@@ -53,89 +57,7 @@ export const getUserRooms = async (userId: string): Promise<Room[]> => {
     const response = await authAPI.get(`/chat/room/get_user_rooms/${userId}/`);
     return response.data;
   } catch (error) {
-    console.error('Error al obtener las salas del usuario:', error);
+    console.error("Error al obtener las salas del usuario:", error);
     throw error;
   }
-};
-
-export const ChatService = {
-  getRecentUsers: async (): Promise<string[]> => {
-    try {
-      const response = await authAPI.get("chat/recent-users/");
-      return response.data;
-    } catch (error) {
-      console.error('Error al obtener los usuarios recientes:', error);
-      throw error;
-    }
-  },
-
-  searchUser: async (query: string): Promise<User | null> => {
-    try {
-      const response = await authAPI.get(`chat/search-user?query=${query}`);
-
-      if (response.data) {
-        return response.data as User;
-      } else {
-        return null;
-      }
-    } catch (error) {
-      console.error('Error al buscar usuario:', error);
-      throw error;
-    }
-  },
-
-  getConversation: async (user: User): Promise<Message[]> => {
-    try {
-      const response = await authAPI.get(`chat/conversations/${user.username}`);
-      const transformedData = response.data.map((item: any) => ({
-        conversation: item.conversation,
-        sender: item.sender,
-        sender_username: item.sender.username,
-        content: item.content
-      }));
-      console.log(response.data.map(item => {
-        return {
-          conversation: item.conversation,
-          sender: item.sender,
-          sender_username: item.sender.username,
-          content: item.content
-        }
-      }))
-      return transformedData;
-
-    } catch (error) {
-      console.error('Error al obtener la conversación:', error);
-      throw error;
-
-    }
-  },
-
-  sendMessage: async (user, message, roomName) => {
-    try {
-      // Establecer la conexión WebSocket
-      const ws = new WebSocket(`ws://localhost:8000/ws/chat/${roomName}/`);
-
-      // Manejar eventos de WebSocket
-      ws.onopen = () => {
-        // Enviar el mensaje al servidor
-        ws.send(JSON.stringify({
-          message,
-          sender: user.id, // Suponiendo que user tiene una propiedad 'id'
-        }));
-      };
-
-      ws.onerror = (error) => {
-        console.error('Error en la conexión WebSocket:', error);
-      };
-
-      ws.onclose = () => {
-        console.log('Conexión WebSocket cerrada');
-      };
-    } catch (error) {
-      console.error('Error al enviar el mensaje:', error);
-      throw error;
-    }
-  },
-
-
 };
